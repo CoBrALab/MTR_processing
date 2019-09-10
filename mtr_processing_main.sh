42bd455de1ed0df2922ff587a730c7e73f74212b
milaurose <47565996+milaurose@users.noreply.github.com>
2019-09-10 19:11:33 -0400

    Update mtr_processing_main.sh

--- mtr_processing_main.sh
+++ mtr_processing_main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #this script is for creating MTR and B1 maps and outputting a final MTR map that is corrected for B1 inhomogeneities. For use on cic computer.
-#as of August 2019, the script works best for scans taken on the room-temperature coil. We are not yet able to correct the surface cryocoil MTR maps due to extensive signal drop-off.
 #TIP: create a csv with the subject ids and paths to each of the input files
 #all input folders should contain raw minc images named with the following convention coil_subjectid_mt.mnc or coil_subjectid_pd.mnc etc.
 #usage:
@@ -50,9 +49,7 @@ minccalc -expression 'acos(A[1]/(2*A[0]))*(180/(4*atan(1)))' $output/preprocesse
 /data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mtr_register_strong.sh $output/denoised/${basename}_b1_60_processed_denoised.mnc $output/denoised/$(basename -s .mnc $2)_denoised.mnc  $output/b1_maps/
 antsApplyTransforms -d 3 -i $output/b1_maps/${basename}_b1_map.mnc -t $output/b1_maps/${basename}_b1_60_processed_denoised.mnc-$(basename -s .mnc $2)_denoised.mnc1_NL.xfm -t $output/b1_maps/${basename}_b1_60_processed_denoised.mnc-$(basename -s .mnc $2)_denoised.mnc0_GenericAffine.xfm -o $output/b1_maps/${basename}_b1_map_registered.mnc --verbose -r $output/denoised/$(basename -s .mnc $2)_denoised.mnc
 
-#NORMALIZE B1!?
-
-#perform the correction-pull the value for m and c from the python script! - is this really necessary, the final script will use a set calibration value
+#perform the correction
 minccalc -expression 'A[0]/(1.31900053*A[1]-0.32723938)' $output/mtr_maps/$(basename -s .mnc $2)_mtr_map_minccalc.mnc $output/b1_maps/b1_map_norm.mnc $output/mtr_maps/$(basename -s .mnc $2)_mtr_map_minccalc_corrected.mnc
 
 #make mtr histograms using no csf mask-requires registration of mt to DSURQE
@@ -60,5 +57,4 @@ mkdir -m a=rwx $output/mtr_histograms
 path_to_no_csf_mask=/data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/tissue_labels/labels_raw_final_no_csf.mnc
 /data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mtr_register_strong.sh $path_to_no_csf_mask $output/denoised/$(basename -s .mnc $2)_denoised.mnc $output/mtr_histograms/ $output/mtr_histograms/
 antsApplyTransforms -d 3 -i $path_to_no_csf_mask -t $output/mtr_histograms/labels_raw_final_no_csf.mnc-$(basename -s .mnc $2)_denoised.mnc1_NL.xfm -t $output/mtr_histograms/labels_raw_final_no_csf.mnc-$(basename -s .mnc $2)_denoised.mnc0_GenericAffine.xfm -o $output/mtr_histograms/labels_no_csf_transformed_to_${basename}.mnc --verbose -r $output/denoised/$(basename -s .mnc $2)_denoised.mnc
-#mincstats -histogram $output/mtr_histograms/$(basename -s .mnc $2)_corrected_hist.txt -mask $output/mtr_histograms/labels_no_csf_transformed_to_${basename}.mnc -mask_binvalue 1 $output/mtr_maps/$(basename -s .mnc $2)_mtr_map_minccalc_corrected.mnc -hist_bins 200 -hist_range -1 1
 mincstats -histogram $output/mtr_histograms/$(basename -s .mnc $2)_hist.txt -mask $output/mtr_histograms/labels_no_csf_transformed_to_${basename}.mnc -mask_binvalue 1 $output/mtr_maps/$(basename -s .mnc $2)_mtr_map_minccalc.mnc -hist_bins 200 -hist_range -1 1
