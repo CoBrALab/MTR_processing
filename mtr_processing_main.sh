b50bb030d2983aebae797a39576e81549680abed
milaurose <47565996+milaurose@users.noreply.github.com>
2019-09-11 22:40:33 -0400

    Update mtr_processing_main.sh

--- mtr_processing_main.sh
+++ mtr_processing_main.sh
@@ -31,10 +31,10 @@ mkdir -m a=rwx $output/masks
 if test -f $output/preprocessed/*${basename}*pd*; then rm $tmp_subject_dir/*pd*; fi
 if test -f $output/preprocessed/*${basename}*b1*60*; then rm $tmp_subject_dir/*b1*; fi
 
-for file in $tmp_subject_dir/*; do /data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mouse-preprocessing-orientation.sh $file $output/preprocessed/$(basename -s .mnc $file)_processed.mnc; done
-for file in $tmp_subject_dir/*; do if [[ "$file" != *b1* ]]; then /data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mouse-preprocessing-denoise-only.sh $file $output/denoised/$(basename -s .mnc $file)_denoised.mnc; fi; done
-/data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mouse-preprocessing-mask.sh $output/preprocessed/$(basename -s .mnc $3)_processed.mnc $output/masks/${basename}_mask.mnc
-for file in $output/preprocessed/*b1*; do /data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/iter_bias_cor.sh $file $output/denoised/$(basename -s .mnc $2)_denoised.mnc $output/masks/${basename}_mask.mnc $output/denoised/$(basename -s .mnc $file)_denoised.mnc; done
+for file in $tmp_subject_dir/*; do mouse-preprocessing-orientation.sh $file $output/preprocessed/$(basename -s .mnc $file)_processed.mnc; done
+for file in $tmp_subject_dir/*; do if [[ "$file" != *b1* ]]; then mouse-preprocessing-denoise-only.sh $file $output/denoised/$(basename -s .mnc $file)_denoised.mnc; fi; done
+mouse-preprocessing-mask.sh $output/preprocessed/$(basename -s .mnc $3)_processed.mnc $output/masks/${basename}_mask.mnc
+for file in $output/preprocessed/*b1*; do iter_bias_cor.sh $file $output/denoised/$(basename -s .mnc $2)_denoised.mnc $output/masks/${basename}_mask.mnc $output/denoised/$(basename -s .mnc $file)_denoised.mnc; done
 
 #create MTR maps
 mkdir -m a=rwx $output/mtr_maps
@@ -46,7 +46,7 @@ mkdir -m a=rwx $output/b1_maps
 minccalc -expression 'acos(A[1]/(2*A[0]))*(180/(4*atan(1)))' $output/preprocessed/$(basename -s .mnc $4)_processed.mnc $output/preprocessed/$(basename -s .mnc $5)_processed.mnc $output/b1_maps/${basename}_b1_map.mnc
 
 #register the b1 map to the MTR map
-/data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mtr_register_strong.sh $output/denoised/${basename}_b1_60_processed_denoised.mnc $output/denoised/$(basename -s .mnc $2)_denoised.mnc  $output/b1_maps/
+mtr_register_strong.sh $output/denoised/${basename}_b1_60_processed_denoised.mnc $output/denoised/$(basename -s .mnc $2)_denoised.mnc  $output/b1_maps/
 antsApplyTransforms -d 3 -i $output/b1_maps/${basename}_b1_map.mnc -t $output/b1_maps/${basename}_b1_60_processed_denoised.mnc-$(basename -s .mnc $2)_denoised.mnc1_NL.xfm -t $output/b1_maps/${basename}_b1_60_processed_denoised.mnc-$(basename -s .mnc $2)_denoised.mnc0_GenericAffine.xfm -o $output/b1_maps/${basename}_b1_map_registered.mnc --verbose -r $output/denoised/$(basename -s .mnc $2)_denoised.mnc
 
 #perform the correction
@@ -55,6 +55,6 @@ minccalc -expression 'A[0]/(1.31900053*A[1]-0.32723938)' $output/mtr_maps/$(base
 #make mtr histograms using no csf mask-requires registration of mt to DSURQE
 mkdir -m a=rwx $output/mtr_histograms
 path_to_no_csf_mask=/data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/tissue_labels/labels_raw_final_no_csf.mnc
-/data/chamal/projects/mila/2019_Magnetization_Transfer/Scans/Piloting/MINC/mtr_register_strong.sh $path_to_no_csf_mask $output/denoised/$(basename -s .mnc $2)_denoised.mnc $output/mtr_histograms/ $output/mtr_histograms/
+mtr_register_strong.sh $path_to_no_csf_mask $output/denoised/$(basename -s .mnc $2)_denoised.mnc $output/mtr_histograms/ $output/mtr_histograms/
 antsApplyTransforms -d 3 -i $path_to_no_csf_mask -t $output/mtr_histograms/labels_raw_final_no_csf.mnc-$(basename -s .mnc $2)_denoised.mnc1_NL.xfm -t $output/mtr_histograms/labels_raw_final_no_csf.mnc-$(basename -s .mnc $2)_denoised.mnc0_GenericAffine.xfm -o $output/mtr_histograms/labels_no_csf_transformed_to_${basename}.mnc --verbose -r $output/denoised/$(basename -s .mnc $2)_denoised.mnc
 mincstats -histogram $output/mtr_histograms/$(basename -s .mnc $2)_hist.txt -mask $output/mtr_histograms/labels_no_csf_transformed_to_${basename}.mnc -mask_binvalue 1 $output/mtr_maps/$(basename -s .mnc $2)_mtr_map_minccalc.mnc -hist_bins 200 -hist_range -1 1
