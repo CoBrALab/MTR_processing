5262e75dc8295f6dadac93f1fb0ceb3ce34937a6
Mila Urosevic <mila.urosevic@mail.mcgill.ca>
2021-08-10 11:14:15 -0400

    Improved syntax

--- publication/sequence_dev/mtr_processing_param_opt.sh
+++ publication/sequence_dev/mtr_processing_param_opt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #this script is for processing MTR data and extracting mean and std values within rois to calculate SNR in dorsal and ventral regions
 #usage:
-#mtr_processing_main.sh output_folder MT_image PD_image mask_for_calc_SNR WM_mask_for_calc_CNR
+#mtr_processing_param_opt.sh output_folder MT_image PD_image mask_for_calc_SNR WM_mask_for_calc_CNR
 
 #name the MT_image in the form ccx_001_mt_6_v1.mnc (where ccx indicates cryocoil, 001 is subjectID, mt_6 refers to the 6th combo
 #of parameters attempted and v1 means that mouse positioning has not been altered since the beginning. If necessary to give a mouse a break outside scanner, v2 will refer to new positioning)
@@ -50,21 +50,27 @@ output=$1
 temp=$(basename $2)
 basename=$(basename $(echo $temp | cut -c1-7)) #extracts the coil_subjectid (assumes that they are in the form xxx_xxx)
 version=$(basename -s .mnc $(echo $temp | cut -d "_" -f 5))
+scan_num=$(basename -s .mnc $(echo $temp | cut -d "_" -f 4))
 
 #create output folders
 mkdir -m a=rwx $output/preprocessed
 mkdir -m a=rwx $output/n4_bias_corrected
+mkdir -m a=rwx $output/transforms_subject_to_DSURQE
 mkdir -m a=rwx $output/masks
 mkdir -m a=rwx $output/transforms_subject_to_DSURQE
-mkdir -m a=rwx $output/subject_specific_tissue_masks
+mkdir -m a=rwx $output/transforms_subject_mt_to_pd
+mkdir -m a=rwx $output/preprocessed_pd_space
 mkdir -m a=rwx $output/mtr_maps
 
-###################################################################Standard MTR processing and map creation ###########################################
+###################################################################Standard processing and mask creation ###########################################
 #first, preprocess all the images (fix orientation)
-for file in $tmp_subject_dir/*; do $scriptdir/helper/mouse-preprocessing-orientation.sh $file $output/preprocessed/$(basename -s .mnc $file)_processed.mnc; done
+#for file in $tmp_subject_dir/*; do $scriptdir/helper/mouse-preprocessing-orientation.sh $file $output/preprocessed/$(basename -s .mnc $file)_processed.mnc; done
 
-#perform N4 bias field correction. The N4 bias corrected acquisitions are necessary for registration to the atlas, and for registering the b1_map to the mtr.
-$scriptdir/helper/mouse-preprocessing-denoise-only.sh $output/preprocessed/$(basename -s .mnc $mt)_processed.mnc $output/n4_bias_corrected/$(basename -s .mnc $mt)_processed_n4corr.mnc
+#perform N4 bias field correction. The N4 bias corrected acquisitions are necessary for registration to the atlas
+#$scriptdir/helper/mouse-preprocessing-denoise-only.sh $output/preprocessed/$(basename -s .mnc $mt)_processed.mnc $output/n4_bias_corrected/$(basename -s .mnc $mt)_processed_n4corr.mnc
+if [ ! -f $output/n4_bias_corrected/*pd*${version}* ]; then
+  $scriptdir/helper/mouse-preprocessing-denoise-only.sh $output/preprocessed/$(basename -s .mnc $pd)_processed.mnc $output/n4_bias_corrected/$(basename -s .mnc $pd)_processed_n4corr.mnc
+fi
 
 #create mask by registering mt file to DSURQE, then transforming DSURQE mask to subject (do this only once for each version)
 if [ ! -f $output/transforms_subject_to_DSURQE/*${basename}_${version}* ]; then
@@ -77,15 +83,23 @@ if [ ! -f $output/transforms_subject_to_DSURQE/*${basename}_${version}* ]; then
       antsApplyTransforms -d 3 -i $atlas_wm_mask_100micron -t [$output/transforms_subject_to_DSURQE/${basename}_${version}-DSURQE_output_0_GenericAffine.xfm,1] -t $output/transforms_subject_to_DSURQE/${basename}_${version}-DSURQE_output_1_inverse_NL.xfm -n GenericLabel -o $output/subject_specific_tissue_masks/${basename}_${version}_mask_wm.mnc --verbose -r $output/n4_bias_corrected/$(basename -s .mnc $mt)_processed_n4corr.mnc
       antsApplyTransforms -d 3 -i $atlas_cc_mask_100micron -t [$output/transforms_subject_to_DSURQE/${basename}_${version}-DSURQE_output_0_GenericAffine.xfm,1] -t $output/transforms_subject_to_DSURQE/${basename}_${version}-DSURQE_output_1_inverse_NL.xfm -n GenericLabel -o $output/subject_specific_tissue_masks/${basename}_${version}_mask_cc.mnc --verbose -r $output/n4_bias_corrected/$(basename -s .mnc $mt)_processed_n4corr.mnc
 fi
-#create MTR maps too
-ImageMath 3 $output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc MTR $output/preprocessed/$(basename -s .mnc $pd)_processed.mnc $output/preprocessed/$(basename -s .mnc $mt)_processed.mnc $output/masks/${basename}_${version}_mask_nocsf.mnc
+
+############################################################### Registration of all MT acquisitions within a single subject (register to the PD) ####################################
+#register mt-w images to the pd-w image for each version (this is because the mouse may have moved from scan to scan as the sessions were long. Still want MTR maps to be meaningful)
+#$scriptdir/helper/antsRegistration_affine_SyN.sh $output/n4_bias_corrected/$(basename -s .mnc $mt)_processed_n4corr.mnc $output/n4_bias_corrected/$(basename -s .mnc $pd)_processed_n4corr.mnc $output/masks_native_space/${basename}_mask_full.mnc $output/transforms_subject_mt_to_pd/$(basename -s .mnc $mt)-pd
+
+#Apply transforms to the processed mt images to bring them all into pd space
+#antsApplyTransforms -d 3 -i $output/preprocessed/$(basename -s .mnc $mt)_processed.mnc -t $output/transforms_subject_mt_to_pd/$(basename -s .mnc $mt)-pd_output_1_NL.xfm -t $output/transforms_subject_mt_to_pd/$(basename -s .mnc $mt)-pd_output_0_GenericAffine.xfm -o $output/preprocessed_pd_space/$(basename -s .mnc $mt)_processed_pd_space.mnc --verbose -r $output/n4_bias_corrected/$(basename -s .mnc $pd)_processed_n4corr.mnc
+
+############################################################MTR map creation using mt acquisitions in PD space #################################
+#ImageMath 3 $output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc MTR $output/preprocessed/$(basename -s .mnc $pd)_processed.mnc $output/preprocessed_pd_space/$(basename -s .mnc $mt)_processed_pd_space.mnc $output/masks/${basename}_${version}_mask_nocsf.mnc
 
 
 ############################################################ Extract mean and std within masks for the purpose of calculating SNR ############################
 if [ ! -f $output/snr_param_opt.csv ]; then
       echo Scan,region,mean_MTR_gm,mean_pd,stddev_background,MTR_wm,file_mtr,file_pd,mask_file,mask_background,mask_wm >> $output/snr_param_opt.csv
-fi
 
+fi
 backgroundmask=/data/chamal/projects/mila/2019_MTR_on_Cryoprobe/2_cryocoil_sequence_dev/raw_data/manually_drawn_rois_for_snr_calculation/SNR_background_labels.mnc
 #Upper ROI
 MTR_top=$(mincstats -mean -quiet -mask  $mask1 -mask_binvalue 3 $output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc)
@@ -99,9 +113,8 @@ mean_pd_bottom=$(mincstats -mean -quiet -mask  $mask1 -mask_binvalue 2 $output/p
 stddev_back=$(mincstats -stddev -quiet -mask  $backgroundmask -mask_binvalue 1 $output/preprocessed/$(basename -s .mnc $pd)_processed.mnc)
 MTR_wm=$(mincstats -mean -quiet -mask  $mask_wm -mask_binvalue 1 $output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc)
 
-echo ${basename},top,$MTR_top,$mean_pd_top,$stddev_back,$MTR_wm,$output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc,$output/preprocessed/$(basename -s .mnc $pd)_processed.mnc,$mask1,$backgroundmask,$mask_wm>> $output/snr_param_opt.csv
-echo ${basename},bottom,$MTR_bottom,$mean_pd_bottom,$stddev_back,$MTR_wm,$output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc,$output/preprocessed/$(basename -s .mnc $pd)_processed.mnc,$mask1,$backgroundmask,$mask_wm >> $output/snr_param_opt.csv
-
+echo ${scan_num},top,$MTR_top,$mean_pd_top,$stddev_back,$MTR_wm,$output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc,$output/preprocessed/$(basename -s .mnc $pd)_processed.mnc,$mask1,$backgroundmask,$mask_wm>> $output/snr_param_opt.csv
+echo ${scan_num},bottom,$MTR_bottom,$mean_pd_bottom,$stddev_back,$MTR_wm,$output/mtr_maps/$(basename -s .mnc $mt)_mtr_map_imagemath.mnc,$output/preprocessed/$(basename -s .mnc $pd)_processed.mnc,$mask1,$backgroundmask,$mask_wm >> $output/snr_param_opt.csv
 
 
 rm -rf $tmp_subject_dir
